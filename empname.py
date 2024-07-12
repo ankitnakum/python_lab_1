a0587d732495e0437075b1f295fde5294ad72653
ankitnakum <nakumankit999@gmail.com>
2024-07-12 13:11:23 +0530

    Add files via upload
    Leb Assigment 1 python

---
+++ empname.py
@@ -0,0 +1,5 @@
+empname=["Ankit","Raj","Karan"]
+empname.insert(1,"Omm")
+empname.remove("Karan")
+empname.append("Jay")
+print(empname)
---
+++ loop.py
@@ -0,0 +1,5 @@
+for x in range(11):
+   print(x)
+   #revers
+for x in range(10,0,-1):
+    print(x)
